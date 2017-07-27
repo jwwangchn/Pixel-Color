82cd9d970f78f623d6015cde802d124082ba3fe3
jwwangchn <jwwangchn@outlook.com>
2017-07-27 20:12:24 +0800

    添加箭头图片

--- main.cpp
+++ main.cpp
@@ -55,7 +55,7 @@ static void onMouse(int event, int x, int y, int f, void *)
 
 int main()
 {
-    src = imread("../image/lena.jpg");
+    src = imread("../image/arrow.jpg");
     imshow(window_name, src);
 
     setMouseCallback(window_name, onMouse, 0);
